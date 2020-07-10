92e96644c3ec82bb0fda4cd2928e8ec35c55d68a
Kenechukwu Nwafor <38596033+Kenechukwu-val@users.noreply.github.com>
2020-07-10 23:21:35 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -6,14 +6,29 @@ import CartContainer from "./components/CartContainer";
 import cartItems from "./cart-items";
 // redux stuff
 
+//Creating store functions from redux
+import {createStore} from 'redux';
+import reducer from './reducer';
+import {Provider} from 'react-redux'
+
+//Creating an intialstore to serve as an argument for the redux store
+const initialStore = {
+  cart: cartItems,
+  total: 0,
+  amount: 0
+}
+
+//A variable which will hold the value
+const store = createStore(reducer, initialStore)
+
 function App() {
   // cart setup
 
   return (
-    <main>
-      <Navbar />
+    <Provider store={store}>
+      <Navbar/>
       <CartContainer cart={cartItems} />
-    </main>
+    </Provider>
   );
 }
 
---
+++ src/reducer.js
@@ -0,0 +1,7 @@
+//A reducer function - accepts two parameters
+const reducer = (state, action) => {
+    
+    return state;
+  }
+
+  export default reducer
