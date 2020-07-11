b61b2ceec5c45734a699217498c5e311a7a33040
Kenechukwu Nwafor <38596033+Kenechukwu-val@users.noreply.github.com>
2020-07-12 00:04:48 +0100

    Added the toggle reducer function

--- src/App.js
+++ src/App.js
@@ -3,23 +3,14 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import CartContainer from "./components/CartContainer";
 // items
-import cartItems from "./cart-items";
-// redux stuff
 
 //Creating store functions from redux
 import {createStore} from 'redux';
 import reducer from './reducer';
 import {Provider} from 'react-redux'
 
-//Creating an intialstore to serve as an argument for the redux store
-const initialStore = {
-  cart: cartItems,
-  total: 105,
-  amount: 0
-}
-
 //A variable which will hold the value
-const store = createStore(reducer, initialStore)
+const store = createStore(reducer)
 
 function App() {
   // cart setup
--- src/reducer.js
+++ src/reducer.js
@@ -1,7 +1,15 @@
-import {CLEAR_CART,REMOVE,INCREASE,DECREASE} from './actions';
+import {CLEAR_CART,REMOVE,GET_TOTALS, TOGGLE_AMOUNT} from './actions';
+import cartItems from "./cart-items";
+
+//Creating an intialstore to serve as an argument for the redux store
+const initialStore = {
+    cart: cartItems,
+    total: 0,
+    amount: 0
+  }
 
 //A reducer function - accepts two parameters
-const reducer = (state, action) => {
+const reducer = (state = initialStore, action) => {
     //CLEAR_CART reducer function
     if ( action.type === CLEAR_CART ) {
         return { ...state, cart: [] }
@@ -14,38 +22,70 @@ const reducer = (state, action) => {
         }
     }
 
-    //INCREASE reducer function
-    if ( action.type === INCREASE ) {
-        let newCart = state.cart.map(cartItem => {
+    // //INCREASE reducer function
+    // if ( action.type === INCREASE ) {
+    //     let newCart = state.cart.map(cartItem => {
+    //         if ( cartItem.id === action.payload.id ) {
+    //             cartItem = { ...cartItem, amount: cartItem.amount + 1 }
+    //         }
+
+    //         return cartItem
+
+    //     })
+
+    //     return {...state, cart: newCart }
+    // }
+
+    // //DECREASE reducer function
+    // if ( action.type === DECREASE ) {
+    //    let newCart = state.cart.map(cartItem => {
+    //         if ( cartItem.id === action.payload.id ) {
+
+    //             cartItem = { ...cartItem, amount: cartItem.amount - 1 }
+    //         }
+
+    //             return cartItem
+
+    //         })
+    //    return { ...state, cart:newCart}
+    // }
+
+
+    //GET_TOTALS reducer function
+    if ( action.type === GET_TOTALS ) {
+        let { total, amount } = state.cart.reduce((cartTotal, cartItem) => {
+            const { price, amount } = cartItem;
+            const itemTotal = price * amount;
+
+
+            cartTotal.total += itemTotal;
+            cartTotal.amount += amount;
+
+            return cartTotal
+        }, 
+        {
+            total: 0,
+            amount: 0
+        })
+
+        total = parseFloat(total.toFixed(2));
+        return {...state, total, amount }
+    }
+
+    if ( action.type === TOGGLE_AMOUNT ) {
+        return { ...state, cart:state.cart.map(cartItem => {
             if ( cartItem.id === action.payload.id ) {
-                cartItem = { ...cartItem, amount: cartItem.amount + 1 }
+                if( action.payload.toggle === "inc") {
+                    cartItem = { ...cartItem, amount: cartItem.amount + 1 }
+                }
+                if( action.payload.toggle === "dec") {
+                    cartItem = { ...cartItem, amount: cartItem.amount - 1 }
+                }
             }
 
             return cartItem
-
-        })
-
-        return {...state, cart: newCart }
-    }
-
-    //DECREASE reducer function
-    if ( action.type === DECREASE ) {
-       let newCart = [];
-       if ( action.payload.amount === 1 ) {
-
-           newCart = state.cart.filter((cartItem) => cartItem.id !== action.payload.id)
-       }
-       else {
-            newCart = state.cart.map(cartItem => {
-                if ( cartItem.id === action.payload.id ) {
-                    cartItem = { ...cartItem, amount: cartItem.amount - 1 }
-                }
-
-                return cartItem
-
-            })
-       }
-       return { ...state, cart:newCart}
+        }
+        )}
     }
     return state;
 
